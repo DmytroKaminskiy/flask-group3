5e0bc7d4b602725de14c2e3603a08444f3cd1d95
dmytro.kaminskiy92@gmail.com <dmytro.kaminskiy92@gmail.com>
2020-08-27 21:14:33 +0300

    generate password view with password-len query parameter

---
+++ utils.py
@@ -0,0 +1,12 @@
+import random
+import string
+
+def generate_random_password(password_len: int=10) -> str:
+    chars = string.digits + \
+            string.ascii_letters + \
+            string.punctuation
+
+    result = ''
+    for _ in range(password_len):
+        result += random.choice(chars)
+    return result
